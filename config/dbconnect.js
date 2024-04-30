2a82a4a24c8bc1067e88f7624749a26a64da074b
Yesudas M J <yesudasmj50@gmail.com>
2024-04-30 13:10:23 +0530

    hosting started 1.2

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_CONNECT);
+    await mongoose.connect("mongodb+srv://WanderBags:WanderBags@cluster0.ddmncoq.mongodb.net/");
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err.message);
